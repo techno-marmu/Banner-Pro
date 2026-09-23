e3a8a792e2a57dd5064d36048dd4d071822486be
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:06 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/alien.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;36m'
+cat <<'ASCII'
+             .-""""-.
+            / -   -  \
+           |  .-. .- |
+           |  \o| |o(|
+           \     ^    /
+            '.  )--' .'
+          .--'      '--.
+         /  /        \  \
+        /__/          \__\
+             ALIEN
+ASCII
+printf '\033[0m\n'
