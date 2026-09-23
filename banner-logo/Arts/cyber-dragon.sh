e7af267df3d87d6fa665843ddd2d83c8f4a16e5f
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:09 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/cyber-dragon.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;35m'
+cat <<'ASCII'
+              /\_/\\
+         ____/ o o \\____
+        /     =^=      \
+       /  __  /|\  __   \
+      /__/  \/ | \/  \___\
+         \___/   \___/
+       == CYBER DRAGON ==
+ASCII
+printf '\033[0m\n'
