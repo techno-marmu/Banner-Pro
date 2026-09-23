fc0f9b539518b927fe66b605c530821567e14fa0
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:19 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/lobster.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;31m'
+cat <<'ASCII'
+          ___     ___
+       __/   \___/   \__
+      /  _           _  \
+     /  / \  _____  / \  \
+    |  |   \/     \/   |  |
+     \  \     ___     /  /
+      \__\___/   \___/__/
+        / /       \ \
+       /_/         \_\
+          LOBSTER
+ASCII
+printf '\033[0m\n'
