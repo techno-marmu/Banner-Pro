993c6f7a059c5ae6751712ec93fdf1f9589744b2
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:17 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/bat.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;35m'
+cat <<'ASCII'
+        /\                 /\
+       /  \___         ___/  \
+      /      \_______/      \
+     /  /\               /\  \
+    /__/  \___       ___/  \__\
+          \  \_____/
+           \__  __/
+              \/
+              BAT
+ASCII
+printf '\033[0m\n'
