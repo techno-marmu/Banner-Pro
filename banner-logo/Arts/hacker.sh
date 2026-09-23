f5f969aaeaac7187f0c3d2ab69662da77a37e526
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:08 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/hacker.sh
@@ -0,0 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;32m'
+cat <<'ASCII'
+        ______________________
+       /                      \
+      |   [ ACCESS GRANTED ]  |
+      |  > root@termux:~# _   |
+      |  > scanning network    |
+      |  > loading payload...  |
+       \______________________/
+             /|  |\
+            /_|__|_\
+              /  \
+             /____\
+ASCII
+printf '\033[0m\n'
