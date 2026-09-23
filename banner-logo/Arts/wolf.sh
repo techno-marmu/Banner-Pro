1bf45ab200d85a02bbbf844b441526da9eacf1cd
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:11 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/wolf.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;37m'
+cat <<'ASCII'
+            /\_/\\
+           / o o  \
+          (   "    )
+           \~(*)~/
+        ____/| |\____
+       /___/ | | \___\
+           W O L F
+ASCII
+printf '\033[0m\n'
