af7634ae802bb31231d3a649530492d4da5a8a8a
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:13 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/matrix-skull.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;32m'
+cat <<'ASCII'
+       010101010101
+      011  SKULL  110
+      01   _____   10
+      1   /     \   0
+      1  | () () |  0
+      0  |  ___  |  1
+      01  \_____/  10
+      0110101010110
+ASCII
+printf '\033[0m\n'
