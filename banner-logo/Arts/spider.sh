7577e886a611771279754af31b916f0b9bfc6d40
CoCo MarMu <marmu.htetaung@gmail.com>
2026-09-23 03:46:16 -0400

    Add Banner-Pro project architecture and example banner files

---
+++ banner-logo/Arts/spider.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+printf '\033[1;31m'
+cat <<'ASCII'
+        \  |  /
+      .-.-.-.-.-.
+   ---/  o     o  \---
+      \    ___    /
+   ----\__/___\__/----
+       /  /   \  \
+      /__/     \__\
+         SPIDER
+ASCII
+printf '\033[0m\n'
